8fb31ab257f0770924fbe110c0d7528c5a714ed8
xdimension <xdim222@gmail.com>
2023-01-20 18:37:54 +0700

    move assets to src

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg'
 import './App.css'
+import "./assets/css/blk-design-system.css"
+import "./assets/css/page.css"
+import "./assets/css/nucleo-icons.css"
 import { Navbar } from './components/Navbar';
+import { Project } from './components/Project';
+import { Footer } from './components/Footer'
 
 function App() {
   return (
-    <Navbar />
-  );
+    <>
+      <Navbar />
+
+      <Project />
+
+      <Footer />
+  </>
+  )
 }
 
 export default App;
