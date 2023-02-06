a6cc1746b14034303bb8a2a4f58934ffc7e5db6d
xdimension <xdim222@gmail.com>
2023-02-06 18:05:09 +0700

    update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,24 @@
+import { useLocation, useHref } from 'react-router-dom'
+import { useContext } from "react"
 import { ConnectToWallet } from "./ConnectToWallet"
+import { AuthContext } from './AuthProvider'
 
 export const Navbar = () => {
+    const location = useLocation()
+
+    console.log(location.pathname)
+    const loginPath = useHref('login')
+
+    const {doSignout} = useContext(AuthContext)
+
+    const signoutHandler = (e) => {
+        e.preventDefault()
+        doSignout()
+    }
 
     return (
         <>
-    <nav className="navbar navbar-absolute navbar-expand-lg navbar-transparent ">
+    <nav className="navbar navbar-absolute navbar-expand-lg navbar-transparent">
         <div className="container">
         <div className="navbar-translate">
             <a className="navbar-brand" href="/" rel="tooltip" title="Homepage" data-placement="bottom" target="_blank">
@@ -18,32 +32,42 @@ export const Navbar = () => {
         </div>
         <div className="collapse navbar-collapse justify-content-end" id="navigation">
             <div className="navbar-collapse-header">
-            <div className="row">
-                <div className="col-6 collapse-brand">
-                <a>
-                    Member Area
-                </a>
+                <div className="row">
+                    <div className="col-6 collapse-brand">
+                        <a>
+                            Member Area
+                        </a>
+                    </div>
+                    <div className="col-6 collapse-close text-right">
+                        <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                            <i className="tim-icons icon-simple-remove"></i>
+                        </button>
+                    </div>
                 </div>
-                <div className="col-6 collapse-close text-right">
-                <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-                    <i className="tim-icons icon-simple-remove"></i>
-                </button>
-                </div>
-            </div>
             </div>
             <ul className="navbar-nav">
-            <li className="nav-item">
-                <a className="nav-link" href="../index.html">Homepage</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="">Purchase</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="">My Account</a>
-            </li>
-            <li className="nav-item">
-                <ConnectToWallet />
-            </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/">Homepage</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/#purchase">Purchase</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/#contact">Contact Us</a>
+                </li>
+                
+                {location.pathname != '/login' && 
+                <>
+                    <li className="nav-item">
+                        <a className="nav-link" href="">My Account</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" onClick={signoutHandler} href="#">Sign Out</a>
+                    </li>
+                    <li className="nav-item">
+                        <ConnectToWallet />
+                    </li>
+                </>}   
             </ul>
         </div>
         </div>
