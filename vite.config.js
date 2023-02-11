fdb2b6fce2398d00f2e02153eecfc037f9d6f75a
xdimension <xdim222@gmail.com>
2023-02-11 14:51:53 +0700

    create chunk for web3 package

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,17 @@ import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('web3')) {
+            return 'web3';
+          }
+        }
+      },
+    }
+  },
   plugins: [
     react(),
     visualizer(),
